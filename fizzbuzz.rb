a0e78b894bee698d64b4f9f53ac3580894774cb3
MishaHerscu <mishaherscu@gmail.com>
2016-06-07 11:35:35 -0400

    fizzbuzz as method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,44 @@
 require 'pry'
 
+# # Let `max_num` be the number that you want to count to.
+# max_num = 30
+# for i in 1.upto(max_num) do
+#
+#   answer_string = ''
+#
+#   if i % 3 == 0
+#     answer_string += 'fizz'
+#   end
+#
+#   if i % 5 == 0
+#     answer_string += 'buzz'
+#   end
+#
+#   if answer_string == ''
+#     answer_string = i
+#   end
+#
+#   puts i.to_s + ' ' + answer_string.to_s
+#
+# end
+
 # Let `max_num` be the number that you want to count to.
-max_num = 30
-for i in 1.upto(max_num) do
 
-  answer_string = ''
-
-  if i % 3 == 0
-    answer_string += 'fizz'
+def fizzbuzz(max_num)
+  for i in 1.upto(max_num) do
+    answer_string = ''
+    if i % 3 == 0
+      answer_string += 'fizz'
+    end
+    if i % 5 == 0
+      answer_string += 'buzz'
+    end
+    if answer_string == ''
+      answer_string = i
+    end
+    puts i.to_s + ' ' + answer_string.to_s
   end
-
-  if i % 5 == 0
-    answer_string += 'buzz'
-  end
-
-  if answer_string == ''
-    answer_string = i
-  end
-
-  puts i.to_s + ' ' + answer_string.to_s
-
 end
+
+binding.pry
+""
